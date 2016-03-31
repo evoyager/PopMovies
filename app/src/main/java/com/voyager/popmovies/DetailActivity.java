c385aaa5272c6476060e661dd9835bc916032583
Evgeniy Gusar <igusar@intropro.com>
2016-03-31 14:18:30 +0300

    JSON parsing to ListView is added.

---
+++ app/src/main/java/com/voyager/popmovies/DetailActivity.java
@@ -0,0 +1,52 @@
+package com.voyager.popmovies;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment())
+                    .commit();
+        }
+    }
+
+    public static class DetailFragment extends Fragment {
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String MOVIES_SHARE_HASHTAG = " #SunshineApp";
+        private String mMovieStr;
+
+        public DetailFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            // The detail Activity called via intent.  Inspect the intent for forecast data.
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                mMovieStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(mMovieStr);
+            }
+
+            return rootView;
+        }
+    }
+}
--- app/src/main/java/com/voyager/popmovies/MoviesFragment.java
+++ app/src/main/java/com/voyager/popmovies/MoviesFragment.java
@@ -1,16 +1,35 @@
 package com.voyager.popmovies;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MoviesFragment extends Fragment {
 
-    public MoviesFragment() {
+    private ArrayAdapter<String> mMoviesAdapter;
 
-    }
+    public MoviesFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +39,163 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        mMoviesAdapter =
+                new ArrayAdapter<String>(
+                  getActivity(),
+                  R.layout.list_item_movie,
+                  R.id.list_item_movie_textview,
+                  new ArrayList<String>());
+
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_movies);
+        listView.setAdapter(mMoviesAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mMoviesAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
+        return rootView;
+    }
+
+    private void updateMovies() {
+        FetchMoviesTask moviesTask = new FetchMoviesTask();
+        moviesTask.execute(10);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Integer, Void, String[]> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        private String[] getMoviesDataFromJson(String moviesJsonStr, Integer numOfMovies)
+                throws JSONException {
+
+            final String RESULTS = "results";
+
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesArray = moviesJson.getJSONArray(RESULTS);
+
+            String[] resultStrs = new String[numOfMovies];
+
+            for(int i = 0; i < resultStrs.length; i++) {
+                String title;
+                Double vote_average;
+
+                JSONObject movie = moviesArray.getJSONObject(i);
+                title = movie.getString("title");
+                vote_average = movie.getDouble("vote_average");
+                resultStrs[i] = title + " " + vote_average;
+            }
+
+            return resultStrs;
+        }
+
+        @Override
+        protected String[] doInBackground(Integer... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            String format = "json";
+            String units = "metric";
+            int numDays = 7;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String MOVIES_BASE_URL =
+                        "http://api.themoviedb.org/3/movie/";
+                final String SORT_METHOD = "top_rated";
+                final String KEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                        .appendPath(SORT_METHOD)
+                        .appendQueryParameter(KEY_PARAM, BuildConfig.MOVIE_DB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+//                Log.v(LOG_TAG, "Pased JSON: " + moviesJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMoviesDataFromJson(moviesJsonStr, params[0]);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mMoviesAdapter.clear();
+                for(String moviesStr : result) {
+                    mMoviesAdapter.add(moviesStr);
+                }
+                // New data is back from the server.  Hooray!
+            }
+        }
     }
 }
